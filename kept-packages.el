9f02bb1d6f155139fe27b96cc44d42ee7c16427a
Andreas Raster <lazor@affenbande.org>
2014-02-07 18:37:07 +0100

    make it into a module

--- kept-packages.el
+++ kept-packages.el
@@ -1,3 +1,5 @@
+(require 'package)
+
 (defvar kept-packages-file-name "~/.emacs.d/kept-packages")
 
 (defun kept-packages-save ()
@@ -8,13 +10,24 @@
 
 (defun kept-packages-load ()
   (with-temp-buffer
-    (insert-file-contents kept-packages-file-name)
-    (while (< (point) (point-max))
-      (let ((package-name (buffer-substring (line-beginning-position)
-                                            (line-end-position))))
-        (unless (package-installed-p (read package-name))
-          (message (concat "installing " package-name))
-          (package-install (read package-name))))
-      (goto-char (line-beginning-position 2)))))
+    (when (condition-case nil (insert-file-contents kept-packages-file-name) (error nil))
+      (while (< (point) (point-max))
+        (let ((package-name (buffer-substring (line-beginning-position)
+                                              (line-end-position))))
+          (unless (package-installed-p (read package-name))
+            (message (concat "installing " package-name))
+            (package-install (read package-name))))
+        (goto-char (line-beginning-position 2))))))
+
+(eval-after-load "kept-packages"
+  '(progn
+     (package-initialize)
+
+     (unless package-archive-contents
+       (package-refresh-contents))
+
+     (kept-packages-load)
+     (add-hook 'after-init-hook
+               'kept-packages-save)))
 
 (provide 'kept-packages)
